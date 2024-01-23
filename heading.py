3bbe094711160cd2762d3d6b8eedda2256587ff3
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-23 17:41:16 +0100

    Update heading.py

--- heading.py
+++ heading.py
@@ -3,17 +3,20 @@ import sys
 
 
 def heading(level, title):
-    # Determine line length based on the option
     spec = {
-        "1": {"len": 80, "style": "\033[1;30;48;2;0;162;255m"},
-        "2": {"len": 65, "style": "\033[1;30;48;2;200;120;255m"},
-        "3": {"len": 50, "style": "\033[1;30;48;2;252;189;0m"},
-        "4": {"len": 35, "style": "\033[30;48;2;79;255;15m"},
+        "1": {"top": 2, "bottom": 1, "len": 110, "style": "\033[1;38;2;255;255;255;48;2;150;0;170m"},
+        "2": {"top": 1, "bottom": 1, "len": 95, "style": "\033[1;38;2;255;255;255;48;2;25;100;175m"},
+        "3": {"top": 1, "bottom": 1, "len": 80, "style": "\033[1;38;2;255;255;255;48;2;100;160;0m"},
+        "4": {"top": 1, "bottom": 0, "len": 65, "style": "\033[1;38;2;255;255;255;48;2;200;150;0m"},
+        "5": {"top": 1, "bottom": 0, "len": 50, "style": "\033[1;38;2;255;255;255;48;2;240;100;0m"},
+        "6": {"top": 1, "bottom": 0, "len": 35, "style": "\033[1;38;2;255;255;255;48;2;220;0;35m"},
     }
     if level not in spec:
         print("Invalid option. Choose between 1 and 4.")
         sys.exit(1)
-    return f"{spec[level]['style']}{title.center(spec[level]['len'])}"
+    margin_top = "\n" * spec[level]['top']
+    margin_bottom = "\n" * spec[level]['bottom']
+    return f"{margin_top}{spec[level]['style']}{title.center(spec[level]['len'])}{margin_bottom}"
 
 
 if __name__ == "__main__":
