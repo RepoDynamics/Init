b6e058f4dec98323ab473a3812695c6e9c4d1f95
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-02 16:43:13 +0100

    Update exception.py

--- src/proman/exception.py
+++ src/proman/exception.py
@@ -5,3 +5,11 @@ class ProManError(Exception):
         self.message = message
         super().__init__(message)
         return
+
+
+class ProManInternalError(ProManError):
+    """An internal error occurred in ProMan."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        return
