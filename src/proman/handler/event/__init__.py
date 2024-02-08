363d9212e70c7ae5d47a2391c22363a8b41643f8
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-08 23:03:43 +0100

    Create __init__.py

---
+++ src/proman/handler/event/__init__.py
@@ -0,0 +1,7 @@
+from proman.handler.event.issue_comment import IssueCommentEventHandler
+from proman.handler.event.issues import IssuesEventHandler
+from proman.handler.event.pull_request import PullRequestEventHandler
+from proman.handler.event.pull_request_target import PullRequestTargetEventHandler
+from proman.handler.event.push import PushEventHandler
+from proman.handler.event.schedule import ScheduleEventHandler
+from proman.handler.event.workflow_dispatch import WorkflowDispatchEventHandler
