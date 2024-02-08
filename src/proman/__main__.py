1cbe59332888b5fa6dbd00897efeb439bbea45c3
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-08 23:03:50 +0100

    Update __main__.py

--- src/proman/__main__.py
+++ src/proman/__main__.py
@@ -1,8 +1,8 @@
 import loggerman
-from repodynamics.exception import RepoDynamicsError
+import controlman
 
 import proman
-from proman.exception import ProManError
+from proman.exception import ProManException
 
 
 if __name__ == "__main__":
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         github=True,
         init_section_number=2,
         exit_code_critical=1,
-        sectioner_exception_catch=(ProManError, RepoDynamicsError),
+        sectioner_exception_catch=(ProManException, controlman.exception.ControlManException),
         sectioner_exception_log_level=loggerman.LogLevel.CRITICAL,
         output_html_filepath="proman_action_log.html",
         root_heading="Execute Action",
