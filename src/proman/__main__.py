9b728b1626b7554723bf8f4fcb72401addbb5309
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-08-28 00:39:29 +0200

    Update __main__.py

--- src/proman/__main__.py
+++ src/proman/__main__.py
@@ -16,4 +16,6 @@ if __name__ == "__main__":
         root_heading="Execute Action",
         html_title="Workflow Log",
     )
+    print("\n\n\nSSSSSSSSSSTTTTARR\n\n\n")
+    loggerman.logger.info("Start")
     proman.run()
