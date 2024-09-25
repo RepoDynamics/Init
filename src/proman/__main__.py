adfac8821dc19fda32c782205a73d52a0d6ef33f
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-25 17:02:59 +0200

    Update __main__.py

--- src/proman/__main__.py
+++ src/proman/__main__.py
@@ -9,6 +9,7 @@ from proman import reporter
 # x = mdit.element.code_block(yaml_str, language="yaml").source("console")
 
 from rich.syntax import Syntax
+from rich.panel import Panel
 from rich.console import Console
 
 yaml_str = """
@@ -17,8 +18,8 @@ examples:
     ex1: 1
     ex2: 2
 """
-x = Syntax(yaml_str, lexer="yaml", code_width=80, dedent=False)
-console = Console(force_terminal=True, width=88, soft_wrap=True)
+x = Panel(Syntax(yaml_str, lexer="yaml"))
+console = Console(force_terminal=True)
 console.print(x)
 
 
