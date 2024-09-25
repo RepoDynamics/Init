8446bbbca55d36d918df679d01099bc49f62f3ee
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-25 16:09:46 +0200

    Update __main__.py

--- src/proman/__main__.py
+++ src/proman/__main__.py
@@ -2,4 +2,18 @@ import proman
 from proman import reporter
 
 reporter.initialize_logger(title_number=[2])
+
+
+import pyserials as ps
+from loggerman import logger
+import mdit
+data = {"fail": False,
+        "run": {
+            "website": True
+        },
+        "website": []
+       }
+yaml_str = ps.write.to_yaml_string(data)
+logger.info("Action Output", mdit.element.code_block(yaml_str, language="yaml"))
+
 proman.run()
