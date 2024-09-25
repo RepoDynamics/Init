ac41d572de9cd256751e48d9c5e7234473489f0f
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-25 17:18:41 +0200

    update

--- src/proman/__main__.py
+++ src/proman/__main__.py
@@ -1,28 +1,5 @@
 import proman
 from proman import reporter
 
-# reporter.initialize_logger(title_number=[2])
-
-
-
-
-# x = mdit.element.code_block(yaml_str, language="yaml").source("console")
-
-from rich.syntax import Syntax
-from rich.panel import Panel
-from rich.console import Console
-
-yaml_str = """
-test: true
-examples:
-    ex1: 1
-    ex2: 2
-"""
-x = Panel(Syntax(yaml_str, lexer="yaml"))
-console = Console(force_terminal=True)
-console.print(x)
-
-
-
-
-# proman.run()
+reporter.initialize_logger(title_number=[2])
+proman.run()
