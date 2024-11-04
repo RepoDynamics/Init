0fe6830e9c6a9abc8406ce9a9d1d39d3a8ca3a9a
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-04 21:06:26 +0100

    Create user_manager.py

---
+++ action/src/proman/user_manager.py
@@ -0,0 +1,113 @@
+from __future__ import annotations as _annotations
+
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+from functools import partial
+
+import pyserials as ps
+from controlman import data_helper, data_validator
+from github_contexts.property_dict import PropertyDict
+
+if _TYPE_CHECKING:
+    from typing import Literal
+    from pathlib import Path
+    from controlman.cache_manager import CacheManager
+    from proman.data_manager import DataManager
+    from pylinks.api.github import GitHub
+    from github_contexts.github.enum import AuthorAssociation
+    from github_contexts.github.payload.object.issue import Issue
+
+
+class UserManager:
+
+    def __init__(
+        self,
+        data_main: DataManager,
+        root_path: Path,
+        github_api: GitHub,
+        cache_manager: CacheManager,
+    ):
+        self._github_api = github_api
+        self._cache_manager = cache_manager
+        self._data_main = data_main
+        self._contributors_path_main = root_path / self._data_main["doc.contributors.path"]
+        self._contributors_main = ps.read.json_from_file(
+            self._contributors_path_main
+        ) if self._contributors_path_main.is_file() else {}
+        return
+
+    def get_from_github_rest_id(
+        self,
+        github_id: int,
+        add_to_contributors: bool = True,
+        update_file: bool = False,
+    ) -> User:
+        for member_id, member_data in self._data_main["team"].items():
+            if member_data.get("github", {}).get("rest_id") == github_id:
+                return User(id=member_id, association="member", data=member_data)
+        for github_user_id, github_user in self._contributors_main.items():
+            if github_user_id == github_id:
+                return User(id=github_id, association="user", data=github_user)
+        data = data_helper.fill_entity(
+            entity={"github": {"rest_id": github_id}},
+            github_api=self._github_api,
+            cache_manager=self._cache_manager,
+            validator=partial(data_validator.validate, schema="entity"),
+        )[0]
+        if add_to_contributors:
+            existing_data = self._contributors_main.setdefault("github", {}).setdefault(github_id, {})
+            ps.update.dict_from_addon(data, existing_data)
+            if update_file:
+                self.write_contributors()
+        return User(id=github_id, association="user", data=data)
+
+    def from_issue_author(self, issue: Issue) -> User:
+        user = self.get_from_github_rest_id(issue.user.id)
+        return User(
+            id=user.id,
+            association=user.association,
+            data=user.as_dict,
+            github_association=issue.author_association,
+        )
+
+    def write_contributors(self):
+        with open(self._contributors_path_main, "w") as contributors_file:
+            contributors_file.write(ps.write.to_json_string(self._contributors_main, sort_keys=True, indent=4))
+        return
+
+
+class User(PropertyDict):
+
+    def __init__(
+        self,
+        id: str | int,
+        association: Literal["member", "user", "external"],
+        data: dict,
+        github_association: AuthorAssociation | None = None,
+    ):
+        data["id"] = id
+        data["association"] = association
+        data["github_association"] = github_association.value.lower() if github_association else None
+        super().__init__(data)
+        return
+
+    @property
+    def changelog_entry(self) -> dict[str, str]:
+        return {"id": self.id, "association": self.association}
+
+    @property
+    def md_name(self) -> str:
+        """The user's name as a markdown link."""
+        name = self._data["name"]["full"]
+        url = (
+            self._data.get("github", {}).get("url")
+            or self._data.get("email", {}).get("url")
+            or self._data.get("linkedin", {}).get("url")
+            or self._data.get("researchgate", {}).get("url")
+            or self._data.get("twitter", {}).get("url")
+            or self._data.get("website", {}).get("url")
+            or self._data.get("orcid", {}).get("url")
+        )
+        if url:
+            return f"[{name}]({url})"
+        return name
