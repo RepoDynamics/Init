9a9342d191bc4cd3978e605e6132c3c34e3f2d2e
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-05 04:39:14 +0100

    Update commit_manager.py

--- action/src/proman/commit_manager.py
+++ action/src/proman/commit_manager.py
@@ -16,7 +16,9 @@ from proman.exception import ProManException
 
 
 if _TYPE_CHECKING:
+    from typing import Sequence, Callable, Literal
     from conventional_commits import ConventionalCommitMessage
+    from proman.user_manager import User
 
 
 class CommitManager:
@@ -49,31 +51,122 @@ class CommitManager:
         )
         return
 
-    def create_from_message(self, message: str) -> Commit:
+    def create_from_msg(self, message: str) -> Commit:
         try:
             msg = self._commit_msg_parser.parse(message)
+            return Commit(
+                writer=self._commit_msg_writer,
+                type=msg.type,
+                scope=msg.scope,
+                description=msg.description,
+                body=msg.body,
+                footer=msg.footer,
+            )
         except Exception as e:
-            msg = message
             _logger.warning(
                 "Commit Message Processing",
                 f"Failed to parse commit message: {e}",
                 message,
             )
-        return Commit(msg)
+            parts = message.split("\n", 1)
+            description = parts[0]
+            body = parts[1] if len(parts) > 1 else ""
+            return Commit(
+                writer=self._commit_msg_writer,
+                description=description,
+                body=body,
+            )
 
-    def create_auto_commit_msg(self, commit_type: str, env_vars: dict | None = None) -> Commit:
-        commit_data = self._data_main[f"commit.auto.{commit_type}"]
+    def create_auto(self, id: str, env_vars: dict | None = None) -> Commit:
+        commit_data = self._data_main[f"commit.auto.{id}"]
         scope = commit_data.get("scope")
         if isinstance(scope, str):
             scope = (scope,)
-        commit_msg = self._commit_msg_writer(
+        return Commit(
+            writer=self._commit_msg_writer,
             type=commit_data["type"],
-            description=self._fill_jinja_templates(commit_data["description"], env_vars=env_vars),
+            description=commit_data["description"],
             scope=scope,
-            body=self._fill_jinja_templates(commit_data.get("body", ""), env_vars=env_vars),
-            footer=self._fill_jinja_templates(commit_data.get("footer", {}), env_vars=env_vars),
+            body=commit_data.get("body"),
+            footer=commit_data.get("footer"),
+            type_description=commit_data.get("type_description"),
+            jinja_env_vars=self._jinja_env_vars | (env_vars or {}),
         )
-        return Commit(msg=commit_msg, type_description=commit_data.get("type_description"))
+
+    def create_release(self, id: str, env_vars: dict | None = None) -> Commit:
+        commit_data = self._data_main[f"commit.release"][id]
+        return Commit(
+            writer=self._commit_msg_writer,
+            type=commit_data["type"],
+            description=commit_data["description"],
+            scope=commit_data.get("scope"),
+            body=commit_data.get("body"),
+            footer=commit_data.get("footer"),
+            action=ReleaseAction(commit_data["action"]) if commit_data.get("action") else None,
+            jinja_env_vars=self._jinja_env_vars | (env_vars or {}),
+        )
+
+    # def _get_commits(self, base: bool = False) -> list[Commit]:
+    #     git = self._git_base if base else self._git_head
+    #     commits = git.get_commits(f"{self._context.hash_before}..{self._context.hash_after}")
+    #     logger.info("Read commits from git history", json.dumps(commits, indent=4))
+    #
+    #     parsed_commits = []
+    #     for commit in commits:
+    #         conv_msg = parser.parse(message=commit["msg"])
+    #         if not conv_msg:
+    #             parsed_commits.append(
+    #                 Commit(
+    #                     **commit, group_data=NonConventionalCommit()
+    #                 )
+    #             )
+    #         else:
+    #             group = self._data_main.get_commit_type_from_conventional_type(conv_type=conv_msg.type)
+    #             commit["msg"] = conv_msg
+    #             parsed_commits.append(Commit(**commit, group_data=group))
+    #     return parsed_commits
+
+
+class Commit:
+
+    def __init__(
+        self,
+        writer: Callable[..., ConventionalCommitMessage],
+        type: str | None = None,
+        scope: Sequence[str] | None = None,
+        description: str | None = None,
+        body: str | None = None,
+        footer: dict | None = None,
+        action: ReleaseAction | None = None,
+        type_description: str | None = None,
+        sha: str | None = None,
+        author: User | None = None,
+        committer: User | None = None,
+        jinja_env_vars: dict | None = None,
+    ):
+        self._writer = writer
+        self.type = type
+        self.scope = scope or []
+        self.description = description or ""
+        self.body = body or ""
+        self.footer = CommitFooter(footer or {})
+        self.action = action
+        self.type_description = type_description
+        self.sha = sha
+        self.author = author
+        self.committer = committer
+        self.jinja_env_vars = jinja_env_vars or {}
+        return
+
+    @property
+    def conv_msg(self) -> ConventionalCommitMessage:
+        return self._writer(
+            type=self.type,
+            scope=self.scope,
+            description=self._fill_jinja_templates(self.description),
+            body=self._fill_jinja_templates(self.body),
+            footer=self._fill_jinja_templates(self.footer.as_dict),
+        )
 
     def _fill_jinja_templates(self, templates: dict | list | str, env_vars: dict | None = None) -> dict | list | str:
 
@@ -84,64 +177,11 @@ class CommitManager:
                 return [recursive_fill(value) for value in template]
             if isinstance(template, str):
                 return jinja2.Template(template).render(
-                    self._jinja_env_vars | {"now": datetime.datetime.now(tz=datetime.UTC)} | (env_vars or {})
+                    self.jinja_env_vars | {"now": datetime.datetime.now(tz=datetime.UTC)} | (env_vars or {})
                 )
             return template
 
         return recursive_fill(templates)
-
-
-class Commit:
-    def __init__(
-        self,
-        msg: str | ConventionalCommitMessage,
-        action: ReleaseAction | None = None,
-        type_description: str | None = None,
-        sha: str | None = None,
-        author: dict | None = None,
-        committer: dict | None = None,
-    ):
-        self.msg = msg
-        self.action = action
-        self.type_description = type_description
-        self.sha = sha
-        self.author = author
-        self.committer = committer
-        return
-
-    @property
-    def type(self) -> str:
-        return "" if isinstance(self.msg, str) else self.msg.type
-
-    @property
-    def scope(self) -> tuple[str, ...]:
-        return tuple() if isinstance(self.msg, str) else self.msg.scope
-
-    @property
-    def description(self) -> str:
-        return self.msg.splitlines()[0] if isinstance(self.msg, str) else self.msg.description
-
-    @property
-    def body(self) -> str:
-        if isinstance(self.msg, str):
-            parts = self.msg.split("\n", 1)
-            return parts[1] if len(parts) > 1 else ""
-        return self.msg.body
-
-    @property
-    def footer(self) -> CommitFooter:
-        return CommitFooter({} if isinstance(self.msg, str) else self.msg.footer)
-
-    @property
-    def summary(self) -> str:
-        return self.description if isinstance(self.msg, str) else self.msg.summary
-
-    @property
-    def footerless(self) -> str:
-        return self.msg if isinstance(self.msg, str) else self.msg.footerless
-
-    def __str__(self):
-        return str(self.msg)
 
 
 class CommitFooter:
@@ -153,6 +193,14 @@ class CommitFooter:
     @property
     def initialize_project(self) -> bool:
         return "initialize-project" in self._data
+
+    @property
+    def squash(self) -> bool | None:
+        return self._data.get("squash")
+
+    @property
+    def publish(self) -> bool | None:
+        return self._data.get("publish")
 
     @property
     def version(self) -> _PEP440SemVer | None:
