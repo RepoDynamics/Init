3dbb528d6a47dfe423e010af12eeb43cdb85ce20
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-18 22:51:27 +0100

    Update __init__.py

--- action/src/proman/manager/release/__init__.py
+++ action/src/proman/manager/release/__init__.py
@@ -36,27 +36,6 @@ class ReleaseManager:
     def zenodo(self) -> ZenodoManager:
         return self._zenodo
 
-    def prepare_release(
-        self,
-        version: PEP440SemVer,
-    ):
-        zenodo_output = None
-        doi = None
-        if self._manager.data["release.zenodo"] and self._manager.zenodo_token:
-            zenodo_response = self._create_zenodo_depo(
-                version=version,
-            )
-            doi = zenodo_response["metadata"]["prereserve_doi"]["doi"]
-            zenodo_output = {
-                "id": zenodo_response["id"],
-                "created": zenodo_response["created"],
-                "doi": doi,
-                "links": zenodo_response["links"],
-            }
-        if data["citation"]:
-            self._prepare_citation_for_release(version=version, doi=doi)
-        return zenodo_output
-
     def calculate_next_version(
         self,
         issue_num: int | str,
@@ -86,54 +65,65 @@ class ReleaseManager:
                 new_ver = f"{ver_base.public.base}{new_pre_phase}{ver_base.public.pre[1]}"
                 return Version(public=PEP440SemVer(new_ver))
             return Version(public=ver_base.public.next_post)
-        next_final_ver = self.next_version(version=ver_base.public, action=action,
-                                           first_public_release=first_public_release)
+        next_final_ver = self._next_final_version(version=ver_base.public, action=action,
+                                                  first_public_release=first_public_release)
         if action is ReleaseAction.POST or deploy_type is IssueStatus.DEPLOY_FINAL:
             return Version(next_final_ver)
         version = f"{next_final_ver.base}{deploy_type.prerelease_type}{issue_num}"
         return Version(PEP440SemVer(version))
 
-    def next_dev_version(
+    def tag_next_dev_version(
         self,
         issue_num: int | str,
         git_base: Git,
         git_head: Git,
         action: ReleaseAction,
     ) -> VersionTag:
-        ver_base = self.latest_version(git=git_base, dev_only=False)
-        ver_head = self.latest_version(git=git_head, dev_only=True)
-        ver_last_base = ver_base.public
-        if ver_last_base.pre:
+        version_head = self.latest_version(git=git_head, dev_only=True)
+        next_dev_version_tag = self.calculate_next_dev_version(
+            version_base=self.latest_version(git=git_base, dev_only=False).public,
+            version_head=version_head.public if version_head else None,
+            issue_num=issue_num,
+            action=action,
+        )
+        return self.tag_version(next_dev_version_tag, git=git_head)
+
+    def calculate_next_dev_version(
+        self,
+        version_base: PEP440SemVer,
+        issue_num: int | str,
+        action: ReleaseAction,
+        version_head: PEP440SemVer | None = None,
+    ) -> VersionTag:
+        if version_base.pre:
             # The base branch is a pre-release branch
-            next_ver = ver_last_base.next_post
+            next_ver = version_base.next_post
             if (
-                ver_head
-                and ver_head.public.release == next_ver.release
-                and ver_head.public.pre == next_ver.pre
-                and ver_head.public.dev is not None
+                version_head
+                and version_head.release == next_ver.release
+                and version_head.pre == next_ver.pre
+                and version_head.dev is not None
             ):
-                dev = ver_head.public.dev + 1
+                dev = version_head.dev + 1
             else:
                 dev = 0
-            next_ver_str = f"{next_ver}.dev{dev}"
+            return self.create_version_tag(PEP440SemVer(f"{next_ver}.dev{dev}"))
+        next_ver = self._next_final_version(version=version_base, action=action, first_public_release=False)
+        next_ver_str = str(next_ver)
+        if action is not ReleaseAction.POST:
+            next_ver_str += f".a{issue_num}"
+        if not version_head:
+            dev = 0
+        elif action is ReleaseAction.POST:
+            if version_head.post is not None and version_head.post == next_ver.post:
+                dev = version_head.dev + 1
+            else:
+                dev = 0
+        elif version_head.pre is not None and version_head.pre == ("a", int(issue_num)):
+            dev = version_head.dev + 1
         else:
-            next_ver = self.next_version(ver_last_base, action)
-            next_ver_str = str(next_ver)
-            if action is not ReleaseAction.POST:
-                next_ver_str += f".a{issue_num}"
-            if not ver_head:
-                dev = 0
-            elif action is ReleaseAction.POST:
-                if ver_head.public.post is not None and ver_head.public.post == next_ver.post:
-                    dev = ver_head.public.dev + 1
-                else:
-                    dev = 0
-            elif ver_head.public.pre is not None and ver_head.public.pre == ("a", int(issue_num)):
-                dev = ver_head.public.dev + 1
-            else:
-                dev = 0
-            next_ver_str += f".dev{dev}"
-        return self.tag_version(PEP440SemVer(next_ver_str), git=git_head)
+            dev = 0
+        return self.create_version_tag(PEP440SemVer(f"{next_ver_str}.dev{dev}"))
 
     def latest_version(
         self,
@@ -183,14 +173,14 @@ class ReleaseManager:
 
     def tag_version(
         self,
-        ver: VersionTag | PEP440SemVer,
+        version: VersionTag | PEP440SemVer,
         env_vars: dict | None = None,
         git: Git | None = None,
     ) -> VersionTag:
-        version_tag = self.create_version_tag(version=ver) if not isinstance(ver, VersionTag) else ver
+        version_tag = self.create_version_tag(version=version) if not isinstance(version, VersionTag) else version
         msg = self._manager.fill_jinja_template(
             self._manager.data["tag.version.message"],
-            {"version": ver} | (env_vars or {}),
+            {"version": version} | (env_vars or {}),
         )
         git = git or self._manager.git
         git.create_tag(tag=str(version_tag), message=msg)
@@ -198,27 +188,6 @@ class ReleaseManager:
 
     def create_version_tag(self, version: PEP440SemVer) -> VersionTag:
         return VersionTag(tag_prefix=self._manager.data["tag.version.prefix"], version=version)
-
-    def _prepare_citation_for_release(
-        self,
-        version: PEP440SemVer,
-        doi: str | None,
-    ):
-        cff_filepath = self._manager.path_repo_target / "CITATION.cff"
-        cff = ps.read.yaml_from_file(path=cff_filepath)
-        cff["date-released"] = datetime.datetime.now().strftime('%Y-%m-%d')
-        cff.pop("commit", None)
-        cff["version"] = str(version)
-        if doi:
-            cff["doi"] = doi
-        ps.write.to_yaml_file(data=cff, path=cff_filepath)
-        return
-
-    # def get_current_dirty_version(self):
-    #     version = versioningit.get_version(
-    #         project_dir=repo_path / data_branch["pkg.path.root"],
-    #         config=data_branch["pkg.build.tool.versioningit"],
-    #     )
 
     @staticmethod
     def _next_prerelease_phase(current_phase: Literal["a", "b", "rc"]) -> Literal["a", "b", "rc"]:
@@ -229,7 +198,9 @@ class ReleaseManager:
         }[current_phase]
 
     @staticmethod
-    def next_version(version: PEP440SemVer, action: ReleaseAction, first_public_release: bool) -> PEP440SemVer:
+    def _next_final_version(
+        version: PEP440SemVer, action: ReleaseAction, first_public_release: bool
+    ) -> PEP440SemVer:
         if first_public_release and version.major == 0:
             return PEP440SemVer("1.0.0")
         if action is ReleaseAction.MAJOR:
@@ -245,3 +216,9 @@ class ReleaseManager:
         if action == ReleaseAction.POST:
             return version.next_post
         return version
+
+    # def get_current_dirty_version(self):
+    #     version = versioningit.get_version(
+    #         project_dir=repo_path / data_branch["pkg.path.root"],
+    #         config=data_branch["pkg.build.tool.versioningit"],
+    #     )
