460451d2b8d20888a52c5a7e92a8ddaff4f4a682
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-18 19:44:12 +0100

    Update __init__.py

--- action/src/proman/manager/__init__.py
+++ action/src/proman/manager/__init__.py
@@ -156,6 +156,10 @@ class Manager:
 
     def add_issue_jinja_env_var(self, issue: Issue):
         issue = copy.deepcopy(issue)
+        logger.info(
+            "Issue copied",
+            logger.inspect(issue)
+        )
         issue["user"] = self.user.from_issue_author(issue)
         self.jinja_env_vars["issue"] = issue
         return issue
